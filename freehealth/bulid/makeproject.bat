a6c0d0482d58e8c8d62d10291e5822e47aac4110
LiuYong <liuyonglog@163.com>
2017-08-12 01:12:02 +0800

    bat file
    Signed-off-by: LiuYong <liuyonglog@163.com>

--- freehealth/bulid/makeproject.bat
+++ freehealth/bulid/makeproject.bat
@@ -1,2 +1,2 @@
-E:\Qt\Qt5.6.1\5.6\msvc2015_64\bin\qmake  ..\freehealth.pro -tp vc -r "CONFIG+=debug debug_without_install" -spec win32-msvc2015 LOW_APPNAME=freediams
+E:\Qt\Qt5.6.1\5.6\msvc2015_64\bin\qmake  ..\freehealth.pro -tp vc -r "CONFIG+=debug debug_without_install" -spec win32-msvc2015 LOWERED_APPNAME=freediams
 pause
